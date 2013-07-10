6908653b34f8580aac191d4678edfa054cd9be67
QuanYuan <quanyuan@seas.upenn.edu>
2013-07-09 17:23:23 -0700

    with edit and delete need modify links after update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,4 +16,30 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
+  def update
+     @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+    #  sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
 end
